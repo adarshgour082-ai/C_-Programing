8a5f2cfd91e03fce1274760eb13fe60a04b5dfff
Adarsh Gour <adarshgour082@gmail.com>
2025-11-20 14:08:43 +0530

    Add circle area calculation in main.c
    Implement a program to calculate the area of a circle based on user input.

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    printf("Hello World \n");
+    /*area of circle code*/
+   int redius, area;
+   printf("enter a value of redius\n");
+   scanf("%d\n",&redius);
+   area = 3.14*redius*redius;
+   printf("value of area %d\n",area);
+
+
+   return 0;
+}
