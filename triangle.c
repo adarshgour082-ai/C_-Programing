78008d02c05faac12e072c61e67c2b2cc59b0e66
Adarsh Gour <adarshgour082@gmail.com>
2025-11-20 14:06:45 +0530

    Add triangle area calculation program

---
+++ triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    float x, y;
+/*area of triangle code*/
+printf("value of base\n");
+scanf("%f",&x);
+printf("value of hieght\n");
+scanf("%f\n",&y);
+printf("area of triangle %f",0.5*x*y);
+return 0;      
+}
