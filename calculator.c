f01575ed56bb282f73aeebb3902c42d27ce9eeb5
Adarsh Gour <adarshgour082@gmail.com>
2025-11-20 13:50:44 +0530

    Add simple calculator functionality in calculator.c
    Implement a simple calculator that performs addition, subtraction, multiplication, division, and modulus operations.

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+    printf("Hello World \n");
+    printf("Hello World \n");
+    //simple calculator code
+    int num1, num2,sum;
+    printf("enter a value of num1 & num2\n");
+    scanf("%d%d\n",&num1,&num2);
+    /*multiples line for sum,minus,multiplication,divied */
+    printf("value of sum %d\n",num1+num2);
+    printf("value of minus %d\n",num1-num2);
+    printf("value of multiplication %d\n",num1*num2);
+    printf("value of divied %d\n",num1/num2);
+    printf("value of modulas% %f",num1%num2);
+
+   return 0;
+
+} 
