cb2ba50d6aeb3edef168e5b449620a4fae0812cb
Adarsh Gour <adarshgour082@gmail.com>
2025-11-20 14:00:13 +0530

    Implement rectangle area calculation
    Fix input prompts and scanf format for rectangle area calculation.

---
+++ index_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+
+/* area of rectangle code*/
+    int a,b,area;
+    a = ("enter a value of length\n");
+    b = ("enter a value of breadth\n");
+    printf("enter a value of a & b\n");
+    scanf("%d%d\n",&a,&b);
+    area = a*b;
+    printf("area of rectangle %d\n",area);
+
+
+   return 0;
+
+} 
